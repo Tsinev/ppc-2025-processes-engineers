622fc67fb1e7624f5522a3007d7f639c6ac0db8a
unknown <timur.sinev2000@mail.ru>
2025-12-25 11:16:32 +0300

    can you work pls???

--- tasks/timur_a_integral/mpi/src/ops_mpi.cpp
+++ tasks/timur_a_integral/mpi/src/ops_mpi.cpp
@@ -19,7 +19,7 @@ bool TimurAIntegralMPI::ValidationImpl() {
 }
 
 bool TimurAIntegralMPI::PreProcessingImpl() {
-  GetOutput() = 0.0;
+ GetOutput() = 0.0;
   return true;
 }
 
@@ -56,7 +56,7 @@ double RunKernel(const TaskData &data, int rank, int size, const Func &f) {
 }  // namespace
 
 bool TimurAIntegralMPI::RunImpl() {
-  int rank = 0;
+   int rank = 0;
   int size = 0;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
